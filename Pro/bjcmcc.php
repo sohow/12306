a4c5fb4fdfe07ab912f21ca53d0ecad3e1e30ae7
zongwen1 <zongwen1@staff.weibo.com>
2017-09-28 15:57:01 +0800

    fi

--- Pro/bjcmcc.php
+++ Pro/bjcmcc.php
@@ -7,7 +7,7 @@
 include_once ("../Tool/Http.php");
 
 class Cron {
-	const LOGIN_SEC = '3aa4fyj1wSk5hp0XHCtW5uIuIPYN9cvLZHA538G7DigpHG32B0TlEgrWVHmUBLtpSiZ0OI8fJz1rt4ykFzaAUDH%2FxjBdfw8V1uyoTWlKVbI4JAnfBzkKuMHndp5I0EnUsM03T%2FBUtYKMXF1vsnXqrQu1Fxs2V9EOtCBZ6kMchxv19CW07aK7%2BIjK45aRmmDnD9Jx%2FNVZQmuyft4CZEj2HRk%2B9A%2FQxe8qBvvG3BCoPXgayBsDu1d5kjELWkMW6AIBIKHAPBYInoAYF0bwB0T6oKQVRZoTvR%2FaG3Y%2FkoTS%2BhTP4xdkISRVadpuX7IsyXcUQzPw%2FgmwP65YFzdcDH8dbkPA3bjqYDFvnS7YXjjBQJam3YalhlRbuwdLVs3ursxiamscD7CoMRd5UJnvNk%2BStKoM1kXqqUEW';
+	const LOGIN_SEC = '';
 
 	const LOGIN_URL = 'https://mobilebj.cn/app/websitepwdLogin?ver=bjservice_and_5.4.0&ef=';
 	const SIGN_URL = 'http://mobilebj.cn:12065/app/querySignInfo?token=';
