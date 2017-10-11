48e0cfad2d7dc0621f12359c1148ef7df8b2b901
root <root@host.localdomain>
2017-10-11 13:22:58 +0800

    fix

--- Pro/bjcmcc.php
+++ Pro/bjcmcc.php
@@ -7,13 +7,17 @@
 include_once (dirname(dirname(__FILE__)) . "/Tool/Http.php");
 
 class Cron {
-	const LOGIN_SEC = '';
+	const LOGIN_SEC = '3aa4fyj1wSk5hp0XHCtW5hT3rww38F1ZGorjbER6f4ZWKI7I9JcZtrTeqhRQvW5HhWsTpqVOqOOIzkPL7KKwRxNyl/2XJ7EYIr67VMUjfldh1mbq23R6EBYTIfy5zuyUDcywFflzZ719pinEImmw5KLsVomxlr6r/BPyCIAplSopZfTSx0Fe2xlp6p0BYrZjAzcCVjsZcoQBwxc9P7OoV8GzUU6boIs/U8mG/QCJOsaKsVf5Se7pG8WcVbOqmvI0b1iLX4CjwgmDo4jg9QvGR57eeCoptC8X2FF3YAJc6sdH2PcvFi4/4ph1hICAPO2jGKq0Is+mrRyLM3myMmOfLSeJuFNjw1VSqcXOZemPuOCca2I0mH4tNk0DnTvX5XFakcPtYWLb0QgzoYeF4pGsSg==';
 
 	const LOGIN_URL = 'https://mobilebj.cn/app/websitepwdLogin?ver=bjservice_and_5.4.0&ef=';
 	const QUERY_SIGN_URL = 'http://mobilebj.cn:12065/app/querySignInfo?token=';
 	const SIGN_URL = 'http://mobilebj.cn:12065/app/signIn?token=';
 	const COUPONS_URL = 'http://mobilebj.cn:12065/app/queryEffCoupon?token=';
 	const EXCHANGE_URL = 'http://mobilebj.cn:12065/app/exchangeCoupon?token=';
+
+    const SHAR_URL = 'http://sc.bj.chinamobile.com/rwtx/task!insertShared.do?channel=0&token=';
+    const QUERY_TIME = 'http://sc.bj.chinamobile.com/rwtx/task!queryPrizeTimes.do?token=';
+    const GET_PRIZE = 'http://sc.bj.chinamobile.com/rwtx/task!doPrize.do?token=';
 
 	public static function start() {
 		$try = 3;
@@ -40,7 +44,18 @@ class Cron {
 				}
 			}
 		}
+
+		self::prize($token);
 	}
+
+	public static function prize($token) {
+        $result = Helper_Http::get(self::SHAR_URL . $token);
+        $result = json_decode($result, true);
+        print_r($result);
+        $result = Helper_Http::get(self::GET_PRIZE . $token);
+        $result = json_decode($result, true);
+        print_r($result);
+    }
 }
 
 Cron::start();
